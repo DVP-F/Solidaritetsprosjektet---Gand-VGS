795c4b851c1f37fac9bfc4ca02e79ae36e9f78eb
DVP-F <149503852+DVP-F@users.noreply.github.com>
2023-12-07 08:18:51 +0100

    New files with new stuff

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//!for(i in document.querySelectorAll("#main-div .header").length){
+//!    const headers = [document.querySelectorAll("header")]
+//!    document.getElementById("boxLeftLinks").innerHTML += '<a href="" id="header"></a><br>'
+//!}
