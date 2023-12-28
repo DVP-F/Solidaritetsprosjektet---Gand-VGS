0f7e060c9bf0814de74f822c262e115a3e0fce53
DVP-F <149503852+DVP-F@users.noreply.github.com>
2023-12-28 19:35:56 +0100

    Updates
    New files, media queries ref, etc

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@
 //!    const headers = [document.querySelectorAll("header")]
 //!    document.getElementById("boxLeftLinks").innerHTML += '<a href="" id="header"></a><br>'
 //!}
+if (window.innerWidth > 721){
+    document.getElementById("bgImgPrimary").attributes("src") = "images/ukraine_flag.gif"
+} else {
+    document.getElementById("bgImgPrimary").attributes("src") = "images/ukraine_flag_vertical.gif"
+}
